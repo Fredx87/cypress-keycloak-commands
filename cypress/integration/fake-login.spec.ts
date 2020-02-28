a8ca1be1a8245a31faff08d821087ea15130d0f0
Fredx <freddy87@gmail.com>
2020-02-28 22:18:17 +0100

    Refactored tests for fakeLogin visit url

--- cypress/integration/fake-login.spec.ts
+++ cypress/integration/fake-login.spec.ts
@@ -2,37 +2,34 @@
 /// <reference types="../../" />
 
 describe("Keycloak Fake Login", () => {
-  describe("without a given path", () => {
-    beforeEach(() => {
-      cy.kcFakeLogin("user");
-    });
+  it("should show user as authenticated", () => {
+    cy.kcFakeLogin("user");
+    cy.get("#output").should("contain.text", "Init Success (Authenticated)");
+  });
 
-    it("should show user as authenticated", () => {
-      cy.get("#output").should("contain.text", "Init Success (Authenticated)");
-    });
+  it("should get user data equal to fixture data", () => {
+    cy.kcFakeLogin("user");
+    cy.get("#output").should("contain.text", "Init Success (Authenticated)");
 
-    it("should get user data equal to fixture data", () => {
-      cy.get("#output").should("contain.text", "Init Success (Authenticated)");
+    cy.findByText("Get Profile").click();
 
-      cy.findByText("Get Profile").click();
-
-      cy.fixture("users/user.json").then((userData: UserData) => {
-        cy.get("#output").should(el => {
-          const value = JSON.parse(el.text());
-          expect(value).to.deep.equal(userData.fakeLogin?.account);
-        });
+    cy.fixture("users/user.json").then((userData: UserData) => {
+      cy.get("#output").should(el => {
+        const value = JSON.parse(el.text());
+        expect(value).to.deep.equal(userData.fakeLogin?.account);
       });
     });
-
   });
-  describe("with a path", () => {
-    beforeEach(() => {
-      cy.kcFakeLogin("user", "#/foobar");
-    });
 
-    it("should show user as authenticated again", () => {
-      cy.get("#output").should("contain.text", "Init Success (Authenticated)");
-    });
+  it("should go to the specified path with hash and show user as authenticated", () => {
+    cy.kcFakeLogin("user", "#/foobar");
+    cy.url().should("be.equal", `${Cypress.config("baseUrl")}/#/foobar`);
+    cy.get("#output").should("contain.text", "Init Success (Authenticated)");
+  });
 
+  it("should go to the specified path without hash and show user as authenticated", () => {
+    cy.kcFakeLogin("user", "index.html");
+    cy.url().should("be.equal", `${Cypress.config("baseUrl")}/index.html`);
+    cy.get("#output").should("contain.text", "Init Success (Authenticated)");
   });
 });
